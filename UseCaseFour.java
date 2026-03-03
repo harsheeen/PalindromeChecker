c6d808f959253db4dc8b77f970faa9afc234961c
harsheeen <harsheenkaur023@gmail.com>
2026-03-03 17:13:13 +0530

    Add: Converted string to CharArray and compared start and end

---
+++ UseCaseFour.java
@@ -0,0 +1,20 @@
+package com.PalindromeChecker;
+/** 
+ * =========================================
+ *MAIN CLASS- UseCaseOne - PalindromeChecker
+ *===========================================
+ *
+ *UseCase 4 : APPLICATION : Reverse the string and compare the input with reversed one
+ *
+ *Description:
+ *----Hardcoded the input string
+ *---Reverse the string and compare the input with reversed one
+ *----If yes- print "Is it Palindrome-True!"
+ *----If no- print "Is it Palindrome-False!"
+ *
+ *@author Harsheen Kaur
+ *@version 4.0
+ * */
+public class UseCaseFour {
+
+}
