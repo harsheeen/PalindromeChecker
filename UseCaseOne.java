bd32fdcfef08e01e6f4a04e060c7936f7b6b53e8
harsheeen <harsheenkaur023@gmail.com>
2026-03-03 16:11:43 +0530

    Add: To print welcome message and version

---
+++ UseCaseOne.java
@@ -0,0 +1,31 @@
+package com.PalindromeChecker;
+
+public class UseCaseOne {
+	/** 
+	 * =========================================
+	 *MAIN CLASS- UseCaseOne - PalindromeChecker
+	 *===========================================
+	 *
+	 *UseCase 1 : APPLICATION 1 : ENTRY AND WELCOME MESSAGE
+	 *
+	 *Description:
+	 *----represents the entry point
+	 *----display the welcome message 
+	 *
+	 *----No palindrome logic is implemented
+	 *
+	 *@author Harsheen Kaur
+	 *@version 1.0
+	 * */
+
+	public static void main(String args[]) {
+		System.out.println( "*=========================================*");
+		System.out.println("*MAIN CLASS- UseCaseOne - PalindromeChecker*");
+		System.out.println("*===========================================*");
+		System.out.println( "*=========================================*");
+		System.out.println("*Version 1*");
+		System.out.println("*===========================================*");
+		
+	}
+
+}
